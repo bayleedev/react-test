3030ffc624f98912b041dac8a029e2d27e93a059
Adam Faur <adam.faur@gmail.com>
2016-08-11 21:15:19 -0500

    Add react example.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
-console.log('rawr')
+const React = require('react')
+const ReactDOM = require('react-dom')
+
+const Container = React.createClass({
+  render: function () {
+    return React.createElement('div', {}, 'meow')
+  }
+})
+
+ReactDOM.render(
+  React.createElement(Container),
+  document.getElementById('container')
+)
